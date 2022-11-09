ebcaa7f78222cf8539b5e75d6be2533b69904ef4
Danil <danilsit@gmail.com>
2022-11-09 14:05:20 +0300

    обавил 5 семинар про инлайн и FSM, код не работатет - надо дорабатывать

---
+++ Lesson4-5-Inline+FSM/src/keyboards/__init__.py
@@ -0,0 +1,5 @@
+from .defaults import commands_dafault_keyboard
+from .defaults import see_commands_default_keyboard
+from .defaults import info_default_keyboard
+
+from .inlines import navigation_items_callback, item_count_callback, basket_callback, get_item_inline_keyboard, basket_keyboard
